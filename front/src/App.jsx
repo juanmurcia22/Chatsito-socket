69962ec13125587b406a8de0f8ffb9b6e8d8c94e
juanmurcia22 <jucamucar@gmail.com>
2026-05-13 09:22:21 -0500

    Establecer nombre de usuario y conexión LAN

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -5,15 +5,16 @@ function App(){
   const[inputMessage, setInputMessage] = useState("");
   const[mensajeRecibido, setMensajeRecibido] = useState([]);  
   const [socket, setSocket] = useState();
+  const [user, setUser] = useState("");
 
   useEffect(()=>{
-    const newSocket = io("http://localhost:3000");
+    const newSocket = io("http://10.30.7.66:3000");
     setSocket(newSocket);
     newSocket.on("mensaje", (msg) => {
       setMensajeRecibido(msg);
 
     });
-
+    setUser(prompt("Ingrese su nombre de usuario"));
     return () => newSocket.disconnect();
   }, [])
 
@@ -21,7 +22,7 @@ function App(){
     e.preventDefault();
     //como se envian los mensajes ...
 
-    socket.emit("mensaje", inputMessage);
+    socket.emit("mensaje", {user, inputMessage});
     setInputMessage("");
   }
   
@@ -33,7 +34,7 @@ function App(){
           Enviar
         </button>
       </form>
-      {mensajeRecibido.map(mensaje => <div>{mensaje}</div>)}
+      {mensajeRecibido.map(mensaje => <div>{mensaje.user}: {mensaje.inputMessage}</div>)}
     </div>
   );}
 export default App;
